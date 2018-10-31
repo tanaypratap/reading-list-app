fe49786e9c0661e6fe86930df3c07d645ccaf85f
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-10-31 17:14:32 +0530

    book scema

---
+++ server/schema/schema.js
@@ -0,0 +1,16 @@
+/**
+ * @description main schema file for graphql
+ */
+
+const graphql = require('graphql')
+
+const { GraphQLObjectType, GraphQLString } = graphql
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
