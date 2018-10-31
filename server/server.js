eaef21ba2480620b569703c7503eccd67b0c33af
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-10-31 16:08:17 +0530

    add docs to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+/**
+ * @description main server file, runs express app and gives graphql as a middleware
+ */
+
 const express = require('express')
 const graphqlIHTTP = require('express-graphql')
 
-const app = express()
 const PORT = 3000
 
+const app = express()
+
+
 app.use('/graphql', graphqlIHTTP({
-    
+
 }))
 
 app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`))
