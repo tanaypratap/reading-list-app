ae9ae1e4c9724673851b4a437bbf113070295306
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-11-05 16:27:39 +0530

    first component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import BookList from './components/BookList'
+
 class App extends Component {
   render() {
     return (
       <div id="Main">
        <h1>Ninja's Reading List</h1>
+       <BookList />
       </div>
     );
   }
