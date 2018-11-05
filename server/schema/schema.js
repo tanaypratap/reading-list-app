b7c6f4d13e70f91475ffe89bd0cc7c6cf8a67874
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-11-05 12:29:09 +0530

    author mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -97,9 +97,30 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args){
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                })
+                return author.save()
+            }
+        }
+    }
+})
+
 /**
  * Declaring the schema and exporting it
  */
 module.exports = new GraphQLSchema({ 
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
