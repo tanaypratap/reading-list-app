742256ebefd5fd95a044143833b30927e913195e
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-11-05 19:20:12 +0530

    form functionality

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,29 +4,39 @@ import { graphql } from "react-apollo";
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+    state = {
+        name: '',
+        genre: '',
+        authorId: ''
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
     render() {
         const { authors, loading } = this.props.data
         return <div>
             {loading && <div> Form loading ...</div>}
-            {!loading && <form id="add-book">
+            {!loading && <form id="add-book" onSubmit={this.submitForm}>
                 <div className="field">
                   <label> Book Name: </label>
-                  <input type="text" />
+                  <input type="text" onChange={ (e) => this.setState({ name: e.target.value })} />
                 </div>
                 <div className="field">
                   <label> Genre: </label>
-                  <input type="text" />
+                    <input type="text" onChange={(e) => this.setState({ genre: e.target.value })} />
                 </div>
                 <div className="field">
                   <label> Author: </label>
-                  <select>
+                    <select onChange={(e) => this.setState({ authorId: e.target.value })}>
                     {authors.map(author => (
                       <option key={author.id} value={author.id}> {author.name} </option>
                     ))}
                   </select>
                 </div>
 
-                <button>+</button>
+                <button type="submit">+</button>
               </form>}
           </div>;
       
