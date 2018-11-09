0ce94e9d8c373bd2cece41a302c9b216067897ea
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2018-11-09 13:41:53 +0530

    running-single-query

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { graphql } from "react-apollo";
 
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
-    
+    state = {
+        selectedBook: null
+    }
     render() {
         const { loading, books } = this.props.data
         return (
@@ -14,9 +17,14 @@ class BookList extends Component {
                 }
                 <ul id="book-list">
                 {
-                    !loading && books.map(book => (<li key={book.id}>{book.name}</li>))
+                    !loading && books.map(book => (<li key={book.id} onClick={ (e) => this.setState({ selectedBook: book.id }) }>{book.name}</li>))
                 }
                 </ul>
+                {
+                    this.state.selectedBook &&
+                    <BookDetails bookId={this.state.selectedBook} />
+                }
+                
              </div >
         );
     }
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -18,6 +18,25 @@ const getBooksQuery = gql`
   }
 `;
 
+const getBookQuery = gql`
+  query($id: ID){
+    book(id: $id){
+      id
+      name
+      genre
+      author{
+        id
+        name
+        age
+        books{
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
 const addBookMutation = gql`
     mutation AddBook($name: String!, $genre: String!, $authorId: ID!){
         addBook(name: $name, genre: $genre, authorId: $authorId) {
@@ -27,4 +46,4 @@ const addBookMutation = gql`
     }
 `;
 
-export { getAuthorsQuery, getBooksQuery, addBookMutation }
+export { getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery }
