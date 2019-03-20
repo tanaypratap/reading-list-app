74fb952be5922c91c8f6904168cc9d050cf069d4
Tanay Pratap <tanaypratap@Tanays-MacBook-Pro.local>
2019-03-20 23:17:40 +0530

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,26 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 // Components
-import BookList from './components/BookList'
-import AddBook from './components/AddBook'
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 // Apollo Client Setup
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-})
+	uri: 'https://reading-list-app-server.tanaypratap.now.sh/graphql',
+});
 
 class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div id="main">
-          <h1> Reading List</h1>
-          <BookList />
-          <AddBook />
-        </div>
-      </ApolloProvider>
-    );
-  }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<div id="main">
+					<h1> Reading List</h1>
+					<BookList />
+					<AddBook />
+				</div>
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,33 +2,37 @@
  * @description main server file, runs express app and gives graphql as a middleware
  */
 
-const express = require('express')
-const graphqlIHTTP = require('express-graphql')
-const mongoose = require('mongoose')
-const env = require('dotenv')
-const cors = require('cors')
+const express = require('express');
+const graphqlIHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const env = require('dotenv');
+const cors = require('cors');
 
-
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
 
 env.load();
 
-const app = express()
+const app = express();
 
 // allow cross-origin request
-app.use(cors())
+app.use(cors());
 
 // connect to mLab DB
 // need to put db string with your creds to run in .env file
-mongoose.connect(process.env.MONGO_CONN_URL);
+mongoose.connect('mongodb://tanay:graphql123@ds151863.mlab.com:51863/graphql-demo');
 mongoose.connection.once('open', () => {
-    console.log('Connection to mLab successful!')
-})
-app.use('/graphql', graphqlIHTTP({
-    schema,
-    graphiql: true
-}))
+	console.log('Connection to mLab successful!');
+});
+
+app.get('/', (req, res) => res.send('Hello World!' + process.env.PORT));
+app.use(
+	'/graphql',
+	graphqlIHTTP({
+		schema,
+		graphiql: true,
+	})
+);
 
 // set port in .env file or env variable as PORT,
 // avoid 3000 as client uses that port
-app.listen(process.env.PORT, () => console.log(`Server is listening at port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`Server is listening at port ${process.env.PORT}`));
